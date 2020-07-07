68721085b16d6b9c76d4db9e454385ddf6ee7559
liampol <liampolnz@gmail.com>
2020-07-08 10:28:01 +1200

    Set up libgdx for desktop.

---
+++ desktop/src/com/nf3/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nf3.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nf3.game.NF3Envr;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NF3Envr(), config);
+	}
+}
