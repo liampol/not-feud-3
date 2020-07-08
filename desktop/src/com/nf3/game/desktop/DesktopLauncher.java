2a9e806ed6f8fd34997f764a134b5d865a82ae31
liampol <liampolnz@gmail.com>
2020-07-08 14:01:13 +1200

    Added comments. Testing git

--- desktop/src/com/nf3/game/desktop/DesktopLauncher.java
+++ desktop/src/com/nf3/game/desktop/DesktopLauncher.java
@@ -6,7 +6,11 @@ import com.nf3.game.NF3Envr;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new NF3Envr(), config);
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration(); // DESKTOP APPLICATION CONFIG
+		new LwjglApplication(new NF3Envr(), config); // NEW DESKTOP INTERFACE (interacts with backend)
+		// Pass in a object of the class which extends from ApplicationAdapter OR implements ApplicationListener
+		// This object is the "client-side" for the programmer, and the methods specifies what happens when 
+		// events occur on the application. e.g. resize() is called when the application is resized AND when
+		// the application is run for the first time.
 	}
 }
